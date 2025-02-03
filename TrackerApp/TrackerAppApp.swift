56a709f8905089faf10b83763fa18bf9cd9bb7ec
joeyjohnson8 <124016216+joeyjohnson8@users.noreply.github.com>
2025-02-03 16:52:04 -0500

    Initial Commit

---
+++ TrackerApp/TrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerAppApp.swift
+//  TrackerApp
+//
+//  Created by Joey Johnson on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct TrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackerAppTests/TrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerAppTests.swift
+//  TrackerAppTests
+//
+//  Created by Joey Johnson on 2025-02-03.
+//
+
+import Testing
+@testable import TrackerApp
+
+struct TrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
